83c391a640fc7febde6014360b70fb32c03136d9
int3ro <int3ro@gmail.com>
2014-03-26 20:09:34 +0100

    Changed to Black.NoTitleBar Theme

--- Project/BikeChallenge/src/main/java/at/xidev/bikechallenge/DriveActivity.java
+++ Project/BikeChallenge/src/main/java/at/xidev/bikechallenge/DriveActivity.java
@@ -1,26 +1,16 @@
 package at.xidev.bikechallenge;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-public class DriveActivity extends ActionBarActivity {
+public class DriveActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
     // TEST ADI
@@ -28,41 +18,5 @@ public class DriveActivity extends ActionBarActivity {
     public void buttonOnClick(View view) {
         Intent intent = new Intent(DriveActivity.this, SocialActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drive, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_drive, container, false);
-            return rootView;
-        }
     }
 }
