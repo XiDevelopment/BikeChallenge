f1d39e159d27d99292d8ff8e033f6b6e2c1149f4
Funkerman <rick.spiegl@gmail.com>
2014-04-13 18:51:26 +0200

    changed from email address to username

--- Project/BikeChallenge/app/src/main/java/at/xidev/bikechallenge/app/LoginActivity.java
+++ Project/BikeChallenge/app/src/main/java/at/xidev/bikechallenge/app/LoginActivity.java
@@ -13,7 +13,6 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build.VERSION;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -46,7 +45,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "dev:dev", "admin:admin",
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -54,7 +53,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
     private UserLoginTask mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
@@ -65,8 +64,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
         setContentView(R.layout.activity_login);
 
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.username);
+        //populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -80,18 +79,18 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button mUsernameSignInButton = (Button) findViewById(R.id.username_sign_in_button);
+        mUsernameSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
 
-        Button mEmailRegisterButton = (Button) findViewById(R.id.email_register_button);
-        mEmailRegisterButton.setOnClickListener(new OnClickListener() {
+        Button mUsernameRegisterButton = (Button) findViewById(R.id.username_register_button);
+        mUsernameRegisterButton.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 register();
             }
         });
@@ -108,6 +107,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
 
     }
 
+    /*
     private void populateAutoComplete() {
         if (VERSION.SDK_INT >= 14) {
             // Use ContactsContract.Profile (API 14+)
@@ -117,7 +117,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
             new SetupEmailAutoCompleteTask().execute(null, null);
         }
     }
-
+    */
 
     /**
      * Attempts to sign in or register the account specified by the login form.
@@ -130,11 +130,11 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
         }
 
         // Reset errors.
-        mEmailView.setError(null);
+        mUsernameView.setError(null);
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
+        String username = mUsernameView.getText().toString();
         String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
@@ -149,13 +149,13 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
         }
 
         // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
             cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+        } else if (!isUsernameValid(username)) {
+            mUsernameView.setError(getString(R.string.error_invalid_username));
+            focusView = mUsernameView;
             cancel = true;
         }
 
@@ -167,31 +167,22 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(username, password);
             mAuthTask.execute((Void) null);
         }
     }
-    private boolean isEmailValid(String email) {
-        //check if email address matches a regex of email addresses
-        return email.matches("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+    private boolean isUsernameValid(String username) {
+        //TODO: check on server if username is valid
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() >= 3;
     }
 
     private void register() {
-        //save the state if logged in or not
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putBoolean("loggedIn", true);
-        editor.commit();
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        //calling finish to prevent back button functionalities
-        finish();
+        //TODO: show register activity or register at server
     }
 
     /**
@@ -310,7 +301,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
                 new ArrayAdapter<String>(LoginActivity.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
-        mEmailView.setAdapter(adapter);
+        mUsernameView.setAdapter(adapter);
     }
 
     /**
@@ -319,11 +310,11 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
-        private final String mEmail;
+        private final String mUsername;
         private final String mPassword;
 
-        UserLoginTask(String email, String password) {
-            mEmail = email;
+        UserLoginTask(String username, String password) {
+            mUsername = username;
             mPassword = password;
         }
 
@@ -340,7 +331,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
 
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
+                if (pieces[0].equals(mUsername)) {
                     // Account exists, return true if the password matches.
                     return pieces[1].equals(mPassword);
                 }
@@ -356,6 +347,17 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
             showProgress(false);
 
             if (success) {
+                //save username and encrypted password
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("username", mUsername);
+                editor.putInt("password", mPassword.hashCode());
+                editor.putBoolean("loggedIn", true);
+                editor.commit();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                //calling finish to prevent back button functionalities
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
