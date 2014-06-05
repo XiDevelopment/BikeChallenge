76db6864964407bf203b791ee6140096bb111674
int3ro <int3ro@gmail.com>
2014-06-05 19:12:36 +0200

    Removed Settings from Options Menu.

--- Project/BikeChallenge/app/src/main/java/at/xidev/bikechallenge/view/MainActivity.java
+++ Project/BikeChallenge/app/src/main/java/at/xidev/bikechallenge/view/MainActivity.java
@@ -100,9 +100,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_log_out) {
+        if (id == R.id.action_log_out) {
             AppFacade.getInstance().logout(this);
 
             Intent intent = new Intent(this, LoginActivity.class);
